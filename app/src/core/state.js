4fc814e2ae41bb11f5ae5701b501a9be05d1ca49
frn <francisco.pensa.web@gmail.com>
2016-06-14 09:37:38 -0300

    update state es6 module style

--- app/src/core/state.js
+++ app/src/core/state.js
@@ -3,62 +3,60 @@ import ObjectPathImmutable from 'object-path-immutable'
 import {diff} from 'deep-diff'
 import Rx from 'rxjs'
 import 'rx-dom'
+ 
+var stateData = {}
+var seed = {state:ObjectPath({}), diff:[]}
+var state$ = new Rx.Subject()
+var state$Diffs = state$
+  .scan(function (acc, chg) {
+    var oldState = acc.state.get()
+    var newState = ObjectPathImmutable
+      .set(oldState, chg.path, chg.value)
+    acc.state = ObjectPath(newState)
+    acc.diff = diff(oldState, newState)
+    return acc
+  }, seed)
+  .do(function(stateDiff){
+    stateData = ObjectPathImmutable
+      .set(stateDiff.state.get())
+  })
+  .map(function (stateDiff) {
+    return stateDiff.diff
+  })
+  .filter(function(diffs){
+    return diffs
+  })
+  .mergeMap(function (diffs) {
+    return Rx.Observable.from(diffs)
+  })
+  .publish()
 
-module.exports = (function () {
-  var stateData = {}
-  var seed = {state:ObjectPath({}), diff:[]}
-  var state$ = new Rx.Subject()
-  var state$Diffs = state$
-    .scan(function (acc, chg) {
-      var oldState = acc.state.get()
-      var newState = ObjectPathImmutable
-        .set(oldState, chg.path, chg.value)
-      acc.state = ObjectPath(newState)
-      acc.diff = diff(oldState, newState)
-      return acc
-    }, seed)
-    .do(function(stateDiff){
-      stateData = ObjectPathImmutable
-        .set(stateDiff.state.get())
-    })
-    .map(function (stateDiff) {
-      return stateDiff.diff
-    })
-    .filter(function(diffs){
-      return diffs
-    })
-    .mergeMap(function (diffs) {
-      return Rx.Observable.from(diffs)
-    })
-    .publish()
+state$Diffs.connect()
 
-  state$Diffs.connect()
-
-  function query (path) {
-    return {
-      on: function (kind) {
-        kind = typeof kind === 'string'
-          ? [kind] : kind
-        return state$Diffs
-          .filter(function (d) {
-            return d.path.join('.') === path &&
-              ~kind.indexOf(d.kind)
-          })
-          .map(function (d) {
-            switch(d.kind){
-              case 'A':
-                return {i:d.index, value:d.item.rhs}
-              case 'D':
-                return undefined
-              default:
-                return d.rhs
-            }
-          })
-      },
-      get value () { return ObjectPath(stateData).get(path) },
-      set value (value) { state$.next({path: path, value: value}) }
-    }
+function query (path) {
+  return {
+    on: function (kind) {
+      kind = typeof kind === 'string'
+        ? [kind] : kind
+      return state$Diffs
+        .filter(function (d) {
+          return d.path.join('.') === path &&
+            ~kind.indexOf(d.kind)
+        })
+        .map(function (d) {
+          switch(d.kind){
+            case 'A':
+              return {i:d.index, value:d.item.rhs}
+            case 'D':
+              return undefined
+            default:
+              return d.rhs
+          }
+        })
+    },
+    get value () { return ObjectPath(stateData).get(path) },
+    set value (value) { state$.next({path: path, value: value}) }
   }
-  window.$tate = query
-  return query
-}())
+}
+window.$tate = query
+export default query
