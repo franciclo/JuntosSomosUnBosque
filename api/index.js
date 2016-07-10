590a68aa2664cfc271b056a518a4499cdf8a18ca
Francisco P <francisco@democracyos.io>
2016-07-10 19:42:56 -0300

    fix statics heroku

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express()
-var path = require('path')
 var config = require('./config')
 var port = process.env.PORT || config.port || 3000
 var mongoose = require('mongoose')
@@ -20,9 +19,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-process.env.PWD = process.cwd()
-
-app.use(express.static(__dirname + '/../app/public')) // path.join(process.env.PWD, '..', 'app', 'public')))
+app.use(express.static('../app/public'))
 
 app.set('superSecret', config.secret)
 app.use(session({ secret: 'klj234 lkj23ñlkfoewut2if jfnf' }))
