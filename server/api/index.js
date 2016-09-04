8b0a91d66cf002daa6ba954acba44706410f2f1c
Francisco P <francisco@democracyos.io>
2016-09-04 13:23:43 -0300

    quitar carga de especies

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app, passport) {
   app.get('/especies', require('./especies'))
-  app.get('/cargar-especies', require('./cargar-especies'))
+  // app.get('/cargar-especies', require('./cargar-especies'))
   app.get('/red', require('./red'))
   app.get('/arboles', require('./arboles'))
 }
